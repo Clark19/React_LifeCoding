6cc6a34b5d53d9c536607ab25599fe9b94e4e83d
Clark19 <clarkgable1@gmail.com>
2021-09-24 23:55:30 +0900

    Add files via upload
    생활코딩 React.js 리액트 프로그래밍 실습 첫 커밋

---
+++ src/App.js
@@ -0,0 +1,62 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    );
+  }
+}
+
+class Subjec extends Component {
+  render(){
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
+      </header>
+    );
+  }
+}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Subjec title="WEB" sub="world Wide Web!!!"></Subjec>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is Hyper Text Markup Language."></Content>
+      </div>
+    );
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Xena</h1>
+
+//     </div>
+//   );
+// }
+
+export default App;
