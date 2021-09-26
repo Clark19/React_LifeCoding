eb11f421040d8d4d473c3fe3dd7b02ca6905e07e
Clark19 <clarkgable1@gmail.com>
2021-09-26 19:51:04 +0900

    3부 React Router DOM 실습

---
+++ src/AppRouterDOM.js
@@ -0,0 +1,107 @@
+/* 3부 React Router DOM 2021.09.26 실습
+ */
+import './App.css';
+import { Component, useState } from 'react';
+import {Route, Switch, Link, NavLink, useParams} from 'react-router-dom';
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      Home...
+    </div>
+  );
+}
+
+
+// 실제론 aJax 로 JSON 가져오는 데이터. 이건 학습용이라 하드 코딩
+let contents = [
+  {id:1, title:'HTML', description:'HTML is...'},
+  {id:2, title:'JS', description:'JS is...'},
+  {id:3, title:'React', description:'React is...'},
+];
+
+function Topics() {
+  let list = [];
+  contents.forEach(item => list.push(<li key={item.id}><NavLink to={'/topics/'+item.id}>{item.title}</NavLink></li>));
+
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        {list}
+      </ul>
+
+      {/* <Switch>
+        <Route path="/topics/1"> HTML is ... </Route>
+        <Route path="/topics/2"> JS is ... </Route>
+        <Route path="/topics/3"> React is ... </Route>
+      </Switch> */}
+
+      {/* 위에 주석 처리된 부분을 아래와 같이 개선 */}
+      <Route path="/topics/:topic_id">
+        <Topic></Topic>
+      </Route>
+    </div>
+  );
+}
+
+const logStyle = 'color:red'
+function Topic() {
+  let params = useParams();
+  console.log('%cparams', logStyle, params, params.topic_id);
+
+  let {topic_id} = params;
+  let selected_topic = {
+    title:'Sorry',
+    description:'Not Found'
+  };
+
+  contents.some(item => {
+    if (item.id === Number(topic_id))
+      selected_topic = item;
+  });
+
+  return (
+    <div>
+      <h3>{selected_topic.title}</h3>
+      {selected_topic.description}
+    </div>
+  );
+}
+
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      Contact...
+    </div>
+  );
+}
+
+function AppRouterDOM() {
+  return (
+    <div className="container">
+      <h1>Hello React Router DOM Example</h1>
+
+      <ul>
+        {/* <NavLink> 네비게이션과 유사함. <Link>에 기능을 조금 더한거. */}
+        <li><NavLink exact to="/">Home</NavLink></li>
+        <li><NavLink to="/topics">Topics</NavLink></li>
+        <li><NavLink to="/contact">Contact</NavLink></li>
+      </ul>
+
+      {/* Switch, exact를 사용하고 아래 순서로 콤포넌트를 배열해야 제대로 작동하면서, 이상한 URL로 접근시 'Not Found' 페이지 띄워주기 수월하다  */}
+      <Switch>
+        <Route exact path="/"><Home></Home></Route>
+        <Route path="/topics"><Topics></Topics></Route>
+        <Route path="/contact"><Contact></Contact></Route>
+        <Route path="/">Not Found</Route>
+      </Switch>
+
+    </div>
+  );
+}
+
+export default AppRouterDOM;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import AppClassVsFunc from './AppClassVsFunc';
+import AppRouterDOM from './AppRouterDOM';
+import {BrowserRouter} from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* 웹 서버 설정 잘 바꿔서 어떤 path 로 들어오든 간에 루트 페이지(폴더?)에 있는
+       html을 서비스 할 수 있다면 <BrowserRouter> 사용하면 됨.
+       그게 안되면 <HashRouter>를 사용하면 됨. 대신 이거 사용하며 
+       주소 뒤에 http://example.com/#/ 또는 http://example.com/#/topic 이런식으로
+      '/#'이 더 붙는다. '/#' 요부분은 웹서버에서 무시한다고 함. React에서만 처리한다고 함 */}
+    <BrowserRouter>
+      <AppRouterDOM /> {/* 3부 React Route DOM 강의 내용 */}
+    </BrowserRouter>
     <AppClassVsFunc /> {/* 2부 클래스 스타일 vs. 함수 스타일 편(hook 포함) 강의 내용 */}
     <App /> {/* 1부 React.js 기초 강의 내용 */}
   </React.StrictMode>,
